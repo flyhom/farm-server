21706a79dd4ae63aee8f194c3849969c62643d1e
flyhom <oppqqs1@gmail.com>
2021-10-18 11:46:52 +0800

    upload file

---
+++ app/Http/Controllers/update_data.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+use \DB;
+
+use Event;
+
+class update_data extends BaseController
+{
+
+    public function update(Request $request)
+    {
+        date_default_timezone_set('Asia/Taipei');
+        $sensor_arr = ['luminance', 'temp', 'humidity', 'soil_temp', 'soil_humid', 'ec', 'ph', 'atp', 'uv', 'rainfall'];
+        $data = $request->all();
+
+        if (!$data) {
+            return response()->json(['status' => 400, 'msg' => "沒有傳送任何資料", 'request' => $request, 'request_data' => $data, 'request_data_content' => $request->getContent()]);
+        }
+        $type = $data["type"];
+        $fileOriginalName = $request->file->getClientOriginalName();
+        $filename = $request->file->storeAs('data', $fileOriginalName);
+        // $originalFile = $request->file('file');
+        // $rows= explode(PHP_EOL, Storage::get($filename));
+        dd($filename);
+        foreach ($rows as $row)
+        {
+            $record = str_getcsv($row);
+        }
+        foreach ($reader as $row) {
+            // Parsing the rows...
+        }
+
+        return response()->json(['status' => 200, 'msg' => "success"]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\data;
 use App\Http\Controllers\data_analytics;
+use App\Http\Controllers\update_data;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,6 +29,6 @@ Route::post('chart', [data::class, 'chart']);
 Route::post('analytics/chart', [data_analytics::class, 'chart']);
 Route::post('analytics/correlation', [data_analytics::class, 'correlation']);
 
-
+Route::post('update', [update_data::class, 'update']);
 
 
